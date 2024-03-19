167ea82af50b8575f319901111d4b48691bfbd80
fede-palazz <fede.palazzi1999@gmail.com>
2024-03-19 10:52:22 +0100

    Optimized code

--- week02/lab/lab01_1.js
+++ week02/lab/lab01_1.js
@@ -4,7 +4,7 @@ function printStrings(strings) {
   for (const s of strings) {
     const len = s.length;
     if (len >= 2) {
-      console.log(`${s.substring(0, 2)}${s.substring(len - 2, len)}`);
+      console.log(`${s.substring(0, 2)}${s.substring(len - 2)}`);
     } else console.log("");
   }
 }
