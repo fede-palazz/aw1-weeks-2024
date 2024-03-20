baf663628c6ffcb32dde2f0c7e860334fead57a9
fede-palazz <fede.palazzi1999@gmail.com>
2024-03-20 19:06:00 +0100

    Added week03 lessons files

---
+++ week03/db-test.js
@@ -0,0 +1,16 @@
+import sqlite from "sqlite3";
+
+const db = new sqlite.Database("questions.sqlite", (err) => {
+  if (err) throw err;
+});
+
+let sql = "SELECT * FROM answer";
+let results = [];
+db.all(sql, (err, rows) => {
+  if (err) throw err;
+  // console.log(rows);
+  for (let row of rows) results.push(row);
+});
+
+console.log("*****");
+for (let r of results) console.log(r); // callback hasn't been executed yet
